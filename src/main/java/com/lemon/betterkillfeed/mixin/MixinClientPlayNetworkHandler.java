e00b668669dcdf9031d91a0ed0cc18d9c73cb30d
MattShaw47 <explosivelemon108@gmail.com>
2025-04-28 11:37:33 -0600

    Fixed totem pops registering twice.

--- src/main/java/com/lemon/betterkillfeed/mixin/MixinClientPlayNetworkHandler.java
+++ src/main/java/com/lemon/betterkillfeed/mixin/MixinClientPlayNetworkHandler.java
@@ -19,14 +19,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ClientPlayNetworkHandler.class)
 public class MixinClientPlayNetworkHandler {
 
-    @Inject(method = "onEntityStatus", at = @At("HEAD"))
+    @Inject(method = "onEntityStatus", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/particle/ParticleManager;addEmitter(Lnet/minecraft/entity/Entity;Lnet/minecraft/particle/ParticleEffect;I)V"))
     private void onEntityStatus(EntityStatusS2CPacket packet, CallbackInfo ci) {
         if (packet.getStatus() == 35) { // 35 = Totem Pop
             MinecraftClient client = MinecraftClient.getInstance();
-
-            // Prevents double pops on singleplayer.
-            if (client.getServer() != null) { return; }
-
             client.execute(() -> {
                 Entity entity = packet.getEntity(client.world);
                 if (entity instanceof PlayerEntity player) {
